6e3478f604d1a154fecc9827a673e7cef854494c
Chandresh Patidar <chandreshpatidar5@gmail.com>
2023-01-26 20:13:50 +0530

    Access and add event listner

---
+++ stopwatch.js
@@ -0,0 +1,12 @@
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const resetButton = document.getElementById('resetButton');
+const time = document.getElementById('time');
+
+function onStartButtonClick(event) {}
+function onStopButtonClick(event) {}
+function onResetButtonClick(event) {}
+
+startButton.addEventListener('click', onStartButtonClick);
+startButton.addEventListener('click', onStopButtonClick);
+startButton.addEventListener('click', onResetButtonClick);
