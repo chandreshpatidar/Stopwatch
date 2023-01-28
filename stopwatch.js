9bd7d6723e638c3a81571cbec8da0fe82df2728d
Chandresh Patidar <chandreshpatidar5@gmail.com>
2023-01-28 19:32:24 +0530

    Add functionality for stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,12 +1,84 @@
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
 const resetButton = document.getElementById('resetButton');
-const time = document.getElementById('time');
+const timerEle = document.getElementById('time');
 
-function onStartButtonClick(event) {}
-function onStopButtonClick(event) {}
-function onResetButtonClick(event) {}
+let hours = 00;
+let minutes = 00;
+let seconds = 00;
+let milliSeconds = 00;
+let isTimerRunning = false;
+
+function timer() {
+  if (isTimerRunning) {
+    milliSeconds++;
+
+    if (milliSeconds == 100) {
+      seconds++;
+      milliSeconds = 0;
+    }
+
+    if (seconds == 60) {
+      minutes++;
+      seconds = 0;
+    }
+
+    if (minutes == 60) {
+      hours++;
+      minutes = 0;
+      seconds = 0;
+    }
+
+    let milliSecondsInString = milliSeconds;
+    let secondsInString = seconds;
+    let minutesInString = minutes;
+    let hoursInString = hours;
+
+    if (milliSeconds < 10) {
+      milliSecondsInString = `0${milliSeconds}`;
+    }
+
+    if (seconds < 10) {
+      secondsInString = `0${seconds}`;
+    }
+
+    if (minutes < 10) {
+      minutesInString = `0${minutes}`;
+    }
+
+    if (hours < 10) {
+      hoursInString = `0${hours}`;
+    }
+
+    timerEle.innerHTML = `${hoursInString} : ${minutesInString} : ${secondsInString} : ${milliSecondsInString}`;
+
+    setTimeout(timer, 10);
+  }
+}
+
+function onStartButtonClick() {
+  isTimerRunning = true;
+  startButton.disabled = true;
+  timer();
+}
+
+function onStopButtonClick() {
+  startButton.disabled = false;
+  isTimerRunning = false;
+}
+
+function onResetButtonClick() {
+  startButton.disabled = false;
+  isTimerRunning = false;
+
+  hours = 00;
+  minutes = 00;
+  seconds = 00;
+  milliSeconds = 00;
+
+  timerEle.innerHTML = '00 : 00 : 00 : 00';
+}
 
 startButton.addEventListener('click', onStartButtonClick);
-startButton.addEventListener('click', onStopButtonClick);
-startButton.addEventListener('click', onResetButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+resetButton.addEventListener('click', onResetButtonClick);
